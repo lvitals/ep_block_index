214a7f5fb9c2d0ca2aacdf109348eaa9515c72a1
Leandro Vital <leandro.santos@ima.sp.gov.br>
2019-02-08 15:02:40 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+exports.blockIndexRoute = function(hook_name, args, cb) {
+	args.app.get("/", function(req, res) {
+		var r = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' + "\n";
+
+		r += '<html>' + "\n";
+		r += '<head>' + "\n";
+		r += '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">' + "\n";
+		r += '</head>' + "\n";
+		r += '<body>' + "\n";
+		r += '</body>' + "\n";
+		r += '</html>' + "\n";
+
+		res.send(r);
+	});
+};
